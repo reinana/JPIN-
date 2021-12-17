dfbb0c40477d8e756663285bed702ed2171b438c
reina <62531225+reinana@users.noreply.github.com>
2021-12-17 14:53:53 +0900

    動画８

--- HelloNetwork/HelloClient.java
+++ HelloNetwork/HelloClient.java
@@ -1,18 +1,49 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class HelloClient {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket server = new Socket("127.0.0.1", 9999);
+        
+        
+        
+        // -----------------8--------------
 
+        Socket server = new Socket("127.0.0.1",9999);
         PrintWriter out = new PrintWriter(server.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        out.println("Hello.");
-        out.println("bye");
-        out.flush();
-
+        String line = null;
+        while((line = br.readLine()) != null) {
+            out.println(line);
+            out.flush();
+        }
+        br.close();
         server.close();
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // ------------7-------------
+        // Socket server = new Socket("127.0.0.1", 9999);
+
+        // PrintWriter out = new PrintWriter(server.getOutputStream());
+
+        // out.println("Hello.");
+        // out.println("bye");
+        // out.flush();
+
+        // server.close();
     }
 }
--- HelloNetwork/HelloServer.java
+++ HelloNetwork/HelloServer.java
@@ -7,21 +7,45 @@ import java.net.Socket;
 
 public class HelloServer {
     public static void main(String[] args) throws IOException {
-        
-        ServerSocket server = new ServerSocket(9999);
+        // ------8---------サーバーを改造する
 
-        Socket socket = server.accept();
+        ServerSocket server = new ServerSocket(9999);
+        Socket socket = server.accept(); // 待ち受け
+
         InputStream is = socket.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        String line = null;
         while(true) {
-            line = br.readLine();
+            String line = br.readLine();
+
+            if(line == null) { // 筒の中に何もないとnullを返すので、nullの時は表示させないように
+                continue;
+            }
+
             if("bye".equals(line)) {
                 break;
             }
             System.out.println(line);
         }
+
+        server.close();
+
+
+
+        // -----------------6-------------------
+        // ServerSocket server = new ServerSocket(9999);
+
+        // Socket socket = server.accept();
+        // InputStream is = socket.getInputStream();
+        // BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        // while(true) {
+        //     String line = br.readLine();
+        //     if("bye".equals(line)) {
+        //         break;
+        //     }
+        //     System.out.println(line);
+        // }
 
     }
 }
